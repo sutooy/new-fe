5dea913a6c1d0c55e054049cbb9993adebf2f72a
movus-takayuki-miwa <takayuki.miwa@mo-vus.com>
2024-01-26 18:25:15 +0900

    fix i18n

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,26 @@
 import { ClientWrapperLayout } from '@/components/layout/client-wrapper'
+import { getTranslation } from '@/i18n'
+import {
+  DEFAULT_NS,
+  KEY_PREFIX_OPTIONS,
+  LANGUAGE_OPTIONS,
+} from '@/i18n/settings'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+
+// metadataを多言語対応
+export async function generateMetadata({
+  params: { lng = LANGUAGE_OPTIONS.ENGLISH },
+}) {
+  const { t, i18n } = await getTranslation(lng, DEFAULT_NS, {
+    keyPrefix: KEY_PREFIX_OPTIONS.metadata,
+  })
+  return {
+    title: t('title'),
+    description: t('description'),
+  }
+}
 
 export default function RootLayout({
   children,
