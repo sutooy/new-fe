177ed945126e5d76bd34cca80c18a8e7cc5065e7
nahokodama <nahokodama@gmail.com>
2024-02-13 12:27:34 +0900

    style:  レイアウトコンポーネントの移動

--- src/components/layout/client-wrapper/index.tsx
+++ src/components/layout/client-wrapper/index.tsx
@@ -1,133 +1,17 @@
 'use client'
-import { Menu } from '@/components/layout/menu/index'
-import { H1Header } from '@/components/shared/h1-header/index'
-import ApploClientProvider from '@/contexts/apolloContext'
 import { AuthProvider } from '@/contexts/authContext'
-import { SnackbarProvider } from '@/contexts/snackbarContext'
 import { ThemeProvider } from '@/contexts/themeContext'
-import mouvsLogo from '@/images/logo-movus-grey.svg'
-import CancelIcon from '@mui/icons-material/Cancel'
-import MenuIcon from '@mui/icons-material/Menu'
-import MuiDrawer from '@mui/material/Drawer'
-import IconButton from '@mui/material/IconButton'
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
-import { Mui, Style } from './index.css'
+import { DefaultLayout } from '../dfault-layout'
 
 type Props = {
   children: React.ReactNode
 }
 
 export const ClientWrapperLayout: React.FC<Props> = ({ children }: Props) => {
-  // i18nの言語推定の反映を待ち、hydration errorを避ける
-  const [initialRenderComplete, setInitialRenderComplete] =
-    useState<boolean>(false)
-
-  useEffect(() => {
-    setInitialRenderComplete(true)
-  }, [])
-
-  const [isFixedNav, serIsFixedNav] = useState(false)
-
-  const windowScroll = () => {
-    serIsFixedNav(window.scrollY > 50)
-  }
-  // アカウントの追従バー
-  useEffect(() => {
-    window.addEventListener('scroll', windowScroll)
-    return () => window.removeEventListener('scroll', windowScroll)
-  }, [])
-
-  // メニュー開閉フラグ
-  const localStorageName = 'minMenu'
-  const isMinMenu = localStorage.getItem(localStorageName)
-  const [isMin, setIsMin] = React.useState(
-    isMinMenu !== null ? JSON.parse(isMinMenu) : false
-  )
-
-  // メニュー開閉トグル
-  const handleIsMin = (isMin: boolean) => {
-    setIsMin(isMin)
-    localStorage.setItem(localStorageName, isMin.toString())
-  }
-
-  if (!initialRenderComplete) return <></>
   return (
     <ThemeProvider>
       <AuthProvider>
-        <SnackbarProvider>
-          <div className={Style.layout}>
-            <MuiDrawer
-              variant="permanent"
-              className={`${Mui.drawer}  ${
-                isMin ? Mui.closedMixin : Mui.openedMixin
-              }`}
-              open={!isMin}
-            >
-              <div className={Mui.drawerHeader}>
-                {/* ロゴ */}
-                {!isMin && (
-                  <div className={Style.logo}>
-                    <Image
-                      src={mouvsLogo}
-                      width={144}
-                      height={23}
-                      alt="movus"
-                    />
-                  </div>
-                )}
-                {/* 開く */}
-                <IconButton
-                  color="inherit"
-                  aria-label="open drawer"
-                  onClick={() => handleIsMin(false)}
-                  edge="start"
-                  sx={{
-                    margin: '15px auto 43px',
-                    ...(!isMin && { display: 'none' }),
-                  }}
-                >
-                  <MenuIcon />
-                </IconButton>
-                {/* 閉じる */}
-                <IconButton
-                  color="inherit"
-                  aria-label="open drawer"
-                  onClick={() => handleIsMin(true)}
-                  edge="start"
-                  sx={{
-                    position: 'absolute',
-                    top: 20,
-                    right: 10,
-                    ...(isMin && { display: 'none' }),
-                  }}
-                >
-                  <CancelIcon />
-                </IconButton>
-              </div>
-              {/* メニュー */}
-              <Menu isMin={isMin} />
-            </MuiDrawer>
-            <div className={Style.main}>
-              <div
-                className={`${Style.account} ${
-                  isFixedNav ? Style.accountFixed : ''
-                } `}
-              >
-                アカウント
-              </div>
-              <div className={Style.mainContent}>
-                <ApploClientProvider>
-                  <H1Header
-                    title="タイトル"
-                    subText="subsubsubsubsubsubsubsubsub"
-                  />
-                  {children}
-                </ApploClientProvider>
-              </div>
-            </div>
-          </div>
-        </SnackbarProvider>
+        <DefaultLayout>{children}</DefaultLayout>
       </AuthProvider>
     </ThemeProvider>
   )
