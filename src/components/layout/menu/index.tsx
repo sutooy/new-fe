92f3b7b34f70fab76b0ec91b4fe2441a4d53568a
nahokodama <nahokodama@gmail.com>
2024-02-13 12:49:52 +0900

    style:  コードフォーマッター

--- src/components/layout/menu/index.tsx
+++ src/components/layout/menu/index.tsx
@@ -1,20 +1,20 @@
 'use client'
-import { PopupMenu, PopupMenuItem } from '@/components/shared/popup-menu/index';
-import { useTranslation } from '@/i18n/client';
-import { NAMESPACE_OPTIONS } from '@/i18n/settings';
-import { Tooltip } from '@material-ui/core';
-import Collapse from '@material-ui/core/Collapse';
-import { SvgIconProps } from '@material-ui/core/SvgIcon';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import GroupsIcon from '@mui/icons-material/Groups';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import { TFunction } from 'i18next';
-import { useRouter } from 'next/dist/client/components/navigation';
-import React, { useState } from 'react';
-import { Style } from './index.css';
+import { PopupMenu, PopupMenuItem } from '@/components/shared/popup-menu/index'
+import { useTranslation } from '@/i18n/client'
+import { NAMESPACE_OPTIONS } from '@/i18n/settings'
+import { Tooltip } from '@material-ui/core'
+import Collapse from '@material-ui/core/Collapse'
+import { SvgIconProps } from '@material-ui/core/SvgIcon'
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import GroupsIcon from '@mui/icons-material/Groups'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
+import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material'
+import { TFunction } from 'i18next'
+import { useRouter } from 'next/dist/client/components/navigation'
+import React, { useState } from 'react'
+import { Style } from './index.css'
 type Props = {
   // 開閉フラグ
   isMin: boolean
@@ -31,17 +31,17 @@ type Menu = {
   path?: string
   icon: React.ReactElement<SvgIconProps>
   disabled: boolean
-  subMenu: SubMenu[],
+  subMenu: SubMenu[]
 }
 
 // ポップアップメニュー用にリンク情報を詰めなおす
 const createLinkList = (subMenu: SubMenu[]): PopupMenuItem[] => {
   const linkList: PopupMenuItem[] = []
   for (const item of subMenu) {
-    if (!item.disabled){
+    if (!item.disabled) {
       linkList.push({
         linkText: item.title,
-        path: item.path
+        path: item.path,
       })
     }
   }
@@ -49,74 +49,80 @@ const createLinkList = (subMenu: SubMenu[]): PopupMenuItem[] => {
 }
 
 // i18nでのメニュー作成
-const createMenu = (menuT: TFunction<string | readonly string[], "metadata">) => {
+const createMenu = (
+  menuT: TFunction<string | readonly string[], 'metadata'>
+) => {
   return [
     {
-      title: menuT("homepage.mainMenu"),
-      path: "/main-menu",
-      icon: <HomeOutlinedIcon className={Style.iconSvg}/>,
+      title: menuT('homepage.mainMenu'),
+      path: '/',
+      icon: <HomeOutlinedIcon className={Style.iconSvg} />,
       disabled: false,
       subMenu: [],
     },
     {
-      title: menuT("dashboard.mainMenu"),
-      path: "/main-menu",
-      icon: <DashboardIcon className={Style.iconSvg}/>,
+      title: menuT('dashboard.mainMenu'),
+      path: '/dashboard',
+      icon: <DashboardIcon className={Style.iconSvg} />,
       disabled: false,
       subMenu: [],
     },
     {
-      title: menuT("customer.mainMenu"),
-      icon: <GroupsIcon className={Style.iconSvg}/>,
-      disabled:false,
+      title: menuT('customer.mainMenu'),
+      icon: <GroupsIcon className={Style.iconSvg} />,
+      disabled: false,
       subMenu: [
         {
-          title: menuT("customer.subMenu.customer"),
-          path: "/customer",
+          title: menuT('customer.subMenu.customer'),
+          path: '/customer',
           disabled: false,
         },
         {
-          title: menuT("customer.subMenu.application"),
-          path: "/application",
+          title: menuT('customer.subMenu.application'),
+          path: '/application',
           disabled: false,
         },
       ],
     },
     {
-      title: menuT("bookingManagement.mainMenu"),
-      icon: <GroupsIcon className={Style.iconSvg}/>,
-      disabled:false,
+      title: menuT('bookingManagement.mainMenu'),
+      icon: <GroupsIcon className={Style.iconSvg} />,
+      disabled: false,
       subMenu: [],
     },
     {
-      title: menuT("vehicleManagement.mainMenu"),
-      icon: <GroupsIcon className={Style.iconSvg}/>,
-      disabled:false,
+      title: menuT('vehicleManagement.mainMenu'),
+      icon: <GroupsIcon className={Style.iconSvg} />,
+      disabled: false,
       subMenu: [
         {
-          title: menuT("vehicleManagement.subMenu.vehicleManagement"),
-          path: "/vehicle",
+          title: menuT('vehicleManagement.subMenu.vehicleManagement'),
+          path: '/vehicle',
           disabled: false,
         },
         {
-          title: menuT("vehicleManagement.subMenu.wirelessGPSList"),
-          path: "/GPS",
+          title: menuT('vehicleManagement.subMenu.wirelessGPSList'),
+          path: '/GPS',
           disabled: false,
         },
-      ]
+      ],
     },
   ]
 }
 
-export const Menu: React.FC<Props> = ( {isMin }: Props) => {
-  const router = useRouter();
+export const Menu: React.FC<Props> = ({ isMin }: Props) => {
+  const router = useRouter()
 
   // メニューリスト
   const { t: menuT } = useTranslation(NAMESPACE_OPTIONS.menu)
   const menuList = createMenu(menuT)
-  
-  // サブメニュー開閉
-  const [openSubMenu, setOpenSubMenu] = useState<boolean[]>(new Array<boolean>(Menu.length).fill(false));
+
+  // サブメニュー開閉フラグ
+  const [openSubMenu, setOpenSubMenu] = useState<boolean[]>(
+    new Array<boolean>(Menu.length).fill(false)
+  )
+
+  // サブメニュー開閉トグル
   const handleClick = (item: Menu, index: number) => {
     if (item.path) {
       router.push(item.path)
@@ -124,8 +130,8 @@ export const Menu: React.FC<Props> = ( {isMin }: Props) => {
     // コピーでないと更新できない
     const newState = openSubMenu.concat()
     newState.splice(index, 1, !newState[index])
-    setOpenSubMenu(newState);
-  };
+    setOpenSubMenu(newState)
+  }
 
   // ツールチップ表示判定
   const isShowTooltip = (menu: Menu): boolean => {
@@ -134,50 +140,57 @@ export const Menu: React.FC<Props> = ( {isMin }: Props) => {
 
   return (
     <div className={Style.menu}>
-      <List
-        component="nav"
-        aria-labelledby="nested-list-subheader" 
-      >
-        {menuList.map((main: Menu, mainIndex: number) => ( 
+      <List component="nav" aria-labelledby="nested-list-subheader">
+        {menuList.map((main: Menu, mainIndex: number) => (
           <div key={main.title}>
             {/* メインメニュー */}
             <ListItem button onClick={() => handleClick(main, mainIndex)}>
-              {isShowTooltip(main) &&
-                <Tooltip title={main.title} arrow placement='right'>
+              {isShowTooltip(main) && (
+                <Tooltip title={main.title} arrow placement="right">
                   <ListItemIcon className={`${Style.icon}`}>
                     {main.icon}
                   </ListItemIcon>
                 </Tooltip>
-              }
-              {!isShowTooltip(main) &&
+              )}
+              {!isShowTooltip(main) && (
                 <ListItemIcon className={`${Style.icon}`}>
                   {!isMin && main.icon}
-                  {(isMin && main.subMenu.length > 0) && 
+                  {isMin && main.subMenu.length > 0 && (
                     <PopupMenu items={createLinkList(main.subMenu)}>
-                    {main.icon}
-                  </PopupMenu>
-                  }
+                      {main.icon}
+                    </PopupMenu>
+                  )}
                 </ListItemIcon>
-              }
-              {!isMin && <ListItemText primary={main.title} className={Style.text} />}
-              {(main.subMenu.length > 0 && !isMin) && (
-                openSubMenu[mainIndex] ? <ExpandLess className={Style.iconSvg} /> : <ExpandMore className={Style.iconSvg} />
               )}
+              {!isMin && (
+                <ListItemText primary={main.title} className={Style.text} />
+              )}
+              {main.subMenu.length > 0 &&
+                !isMin &&
+                (openSubMenu[mainIndex] ? (
+                  <ExpandLess className={Style.iconSvg} />
+                ) : (
+                  <ExpandMore className={Style.iconSvg} />
+                ))}
             </ListItem>
             {/* サブメニュー */}
-            {!isMin &&
-              <Collapse in={openSubMenu[mainIndex]} timeout="auto" unmountOnExit>
+            {!isMin && (
+              <Collapse
+                in={openSubMenu[mainIndex]}
+                timeout="auto"
+                unmountOnExit
+              >
                 <List component="div" disablePadding>
                   {main.subMenu.map((sub: SubMenu, index: number) => (
                     <ListItem
                       key={sub.title}
                       button
                       sx={{
-                        "&.MuiListItem-root": {
+                        '&.MuiListItem-root': {
                           width: '100%',
                           maxWidth: 360,
-                          paddingLeft: "46px"
-                        }
+                          paddingLeft: '46px',
+                        },
                       }}
                     >
                       <ListItemText primary={sub.title} />
@@ -185,7 +198,7 @@ export const Menu: React.FC<Props> = ( {isMin }: Props) => {
                   ))}
                 </List>
               </Collapse>
-            }
+            )}
           </div>
         ))}
       </List>
