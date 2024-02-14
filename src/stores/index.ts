6c9908aae8d399816305f246611594f8099885a5
ysaito06 <y.saito@kuga.tech>
2024-02-15 00:22:24 +0900

    Delete auth, index, reducers, langReducer, and store files

--- src/stores/index.ts
+++
@@ -1,3 +0,0 @@
-export * as Action from './action/index'
-export * from './reducers/index'
-export * from './store'
--- src/stores/store.ts
+++
@@ -1,8 +0,0 @@
-// import { createStore, applyMiddleware } from 'redux';
-import { applyMiddleware, legacy_createStore as createStore } from 'redux'
-import thunk from 'redux-thunk'
-import reducers from './reducers/index'
-
-const store = createStore(reducers, {}, applyMiddleware(thunk))
-
-export default store
