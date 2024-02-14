3fbf85abd73845d252e55e9d2bbd683d27fc9431
ysaito06 <y.saito@kuga.tech>
2024-02-15 00:23:14 +0900

    Remove cookie-related functions and update validation schemas

--- src/utils/cookies/setCookie.ts
+++
@@ -1,8 +0,0 @@
-function setCookie(cName: string, cValue: string, expDays: number) {
-  let date = new Date()
-  date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000)
-  const expires = 'expires=' + date.toUTCString()
-  document.cookie = cName + '=' + cValue + '; ' + expires + '; path=/'
-}
-
-export default setCookie
--- src/validations/validationSchema.ts
+++ src/validations/validationSchema.ts
@@ -1,13 +1,25 @@
+import {
+  MutationForgotPasswordArgs,
+  MutationLoginArgs,
+} from '@/generated/graphql'
 import * as yup from 'yup'
 
 /* regex password can't start or end with blank space */
 const validPass = /^\S.*\S.*\S$/
 
-export const validation_login_v2 = yup.object().shape({
-  /* LOGIN v2*/
-  username: yup.string().required('Enter your Username'),
-  password: yup
-    .string()
-    .required('Enter a password')
-    .matches(validPass, 'Password can’t start or end with a blank space'),
-})
+export const validation_login: yup.SchemaOf<MutationLoginArgs> = yup
+  .object()
+  .shape({
+    /* LOGIN v2*/
+    username: yup.string().required('Enter your Username'),
+    password: yup
+      .string()
+      .required('Enter a password')
+      .matches(validPass, 'Password can’t start or end with a blank space'),
+  })
+
+export const validation_email: yup.SchemaOf<MutationForgotPasswordArgs> = yup
+  .object()
+  .shape({
+    email: yup.string().email('Enter a valid email').required('Enter an email'),
+  })
