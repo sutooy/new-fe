09e3aa3ff7dd9a48cb2a4662fe15054c1f871bd4
ysaito06 <y.saito@kuga.tech>
2024-02-15 00:24:28 +0900

    update folder composition

--- src/components/layout/client-wrapper/index.tsx
+++ src/components/layout/client-wrapper/index.tsx
@@ -3,9 +3,7 @@ import ApploClientProvider from '@/contexts/apolloContext'
 import { AuthProvider } from '@/contexts/authContext'
 import { SnackbarProvider } from '@/contexts/snackbarContext'
 import { ThemeProvider } from '@/contexts/themeContext'
-import store from '@/stores/store'
 import React, { useEffect, useState } from 'react'
-import { Provider } from 'react-redux'
 
 type Props = {
   children: React.ReactNode
@@ -22,14 +20,12 @@ export const ClientWrapperLayout: React.FC<Props> = ({ children }: Props) => {
 
   if (!initialRenderComplete) return <></>
   return (
-    <Provider store={store}>
-      <ThemeProvider>
-        <AuthProvider>
-          <SnackbarProvider>
-            <ApploClientProvider>{children}</ApploClientProvider>
-          </SnackbarProvider>
-        </AuthProvider>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider>
+      <AuthProvider>
+        <SnackbarProvider>
+          <ApploClientProvider>{children}</ApploClientProvider>
+        </SnackbarProvider>
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
